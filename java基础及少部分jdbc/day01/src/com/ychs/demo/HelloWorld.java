862d3762ad5053c3fc5593cd1d541f37890c6371
sheng <87010771+yang12sheng@users.noreply.github.com>
2023-10-23 10:55:14 +0800

    Add files via upload

---
+++ java基础及少部分jdbc/day01/src/com/ychs/demo/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.ychs.demo;
+
+/**
+ * @author Lenovo
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world.ÄãºÃ..");
+		A a=new A();
+		a.f();
+	}
+	}
+
+class A{
+	public void f() {
+		System.out.println("hello");
+	}
+}
